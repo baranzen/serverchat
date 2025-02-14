201cf24787fa6273bcaceeb458d45f134568e491
Baran <baranzengeralp@gmail.com>
2025-02-15 02:30:05 +0300

    Remove node_modules from tracking

--- src/messages/dto/create-message.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, MinLength } from "class-validator";
-import { Message } from "../entities/message.entity";
-
-// we are extending the Message entity to create a new message, because we want to use the sender and message properties and dont want to duplicate them.
-export class CreateMessageDto extends Message {}
-
-
